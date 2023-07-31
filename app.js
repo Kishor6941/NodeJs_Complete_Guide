c65061fa85621ef2be748fe7cfee4f34b4ec661b
Kishor6941 <phawadeks9049@gmail.com>
2023-07-31 22:08:10 +0530

    readline

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("First Node Js program")
+const readLine = require('readline');
+const r1 = readLine.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+r1.question('Please enter your name :> ',(name) => {
+    console.log('You entered the',name);
+    r1.close()
+})
+
+r1.on('close',
+() => {
+    console.log("Interface closed")
+})
---
+++ redme.md
@@ -0,0 +1,2 @@
+Node JS Modules 
+readline -> to read the user input based upon that you can create interface, close the Interface
